880dff3aa88d61806f23cae70449ccf64e476ddd
YiQiu <binzixiang@163.com>
2024-08-24 22:22:10 +0800

    feat: common and l10n: Add get localization dynamic_color screenutil

---
+++ lib/common/utils/screen_utils.dart
@@ -0,0 +1,25 @@
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class ScreenUtils {
+  // 屏幕适配工具
+  static final ScreenUtil _screenUtil = ScreenUtil();
+
+  /// 适配屏幕所需的长度
+  static double length({
+    required double vertical,
+    required double horizon,
+  }) {
+    return _screenUtil.orientation.index == 0 ? vertical : horizon;
+  }
+
+  /// 获取横屏还是竖屏
+  /// true: 竖屏  false: 横屏
+  static bool getOrientation() {
+    return _screenUtil.orientation.index == 0 ? true : false;
+  }
+
+  /// 横竖屏返回指定内容
+  static T? byOrientationReturn<T>({T? vertical, T? horizon}) {
+    return _screenUtil.orientation.index == 0 ? vertical : horizon;
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,10 @@
+import 'package:dynamic_color/dynamic_color.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:get/get.dart';
+
+import 'generated/l10n.dart';
 
 void main() {
   runApp(const MyApp());
@@ -7,64 +13,37 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    return DynamicColorBuilder(
+      builder: (ColorScheme? lightDynamic, ColorScheme? darkDynamic) {
+        return ScreenUtilInit(
+          designSize: const Size(720, 1080),
+          splitScreenMode: true,
+          ensureScreenSize: true,
+          builder: (BuildContext context, Widget? child) {
+            return GetMaterialApp(
+              title: 'Flutter Demo',
+              theme: ThemeData(
+                colorScheme: lightDynamic,
+                useMaterial3: true,
+              ),
+              darkTheme: ThemeData(
+                colorScheme: darkDynamic,
+                useMaterial3: true,
+              ),
+              localizationsDelegates: const [
+                S.delegate,
+                GlobalMaterialLocalizations.delegate,
+                GlobalWidgetsLocalizations.delegate,
+                GlobalCupertinoLocalizations.delegate,
+              ],
+              supportedLocales: S.delegate.supportedLocales,
+              home: const Scaffold(),
+            );
+          },
+        );
+      },
     );
   }
 }
