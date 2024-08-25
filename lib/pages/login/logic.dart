5c09716c4231e3f4d0f20f3301e7c75c64cc50f6
YiQiu <binzixiang@163.com>
2024-08-25 19:27:18 +0800

    feat: pages: Complete the development of the landing page

--- lib/pages/login/logic.dart
+++ lib/pages/login/logic.dart
@@ -1,7 +1,55 @@
+import 'package:flutter_login/flutter_login.dart';
 import 'package:get/get.dart';
+import 'package:schedule/common/api/schedule/schedule_user_api.dart';
 
+import '../../generated/l10n.dart';
+import '../../global_logic.dart';
 import 'state.dart';
 
 class LoginLogic extends GetxController {
   final LoginState state = LoginState();
+  final globalLogic = Get.find<GlobalLogic>();
+  final globalState = Get.find<GlobalLogic>().state;
+
+  ScheduleUserApi userApi = ScheduleUserApi();
+
+  /// 登录教务系统
+  Future<String?>? loginEducationalSystem(LoginData data) async {
+    // 开始登录并超时时间为10s
+    final loginStatus = await userApi.loginEducationalSystem(
+      userAccount: data.name,
+      userPassword: data.password,
+    );
+
+    switch (loginStatus) {
+      case ScheduleUserStatus.loginTimeOut:
+        return S.current.login_timeout;
+      case ScheduleUserStatus.success:
+        // 保存用户信息
+        globalLogic.setScheduleUserInfo("username", data.name);
+        globalLogic.setScheduleUserInfo("password", data.password);
+
+        // 储存登录成功
+        globalLogic.setIsLogin(true);
+        return null;
+      case ScheduleUserStatus.fail:
+        return S.current.login_fail;
+    }
+  }
+
+  /// 用户名输入框验证
+  String? userValidator(String? value) {
+    if (value == null || value.isEmpty) {
+      return S.current.login_user_not_empty;
+    }
+    return null;
+  }
+
+  /// 密码输入框验证
+  String? passwordValidator(String? value) {
+    if (value == null || value.isEmpty) {
+      return S.current.login_password_not_empty;
+    }
+    return null;
+  }
 }
--- lib/pages/login/view.dart
+++ lib/pages/login/view.dart
@@ -1,6 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_login/flutter_login.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
+import 'package:schedule/common/utils/logger_utils.dart';
+import 'package:schedule/common/utils/screen_utils.dart';
 
+import '../../generated/l10n.dart';
+import '../app_main/logic.dart';
 import 'logic.dart';
 
 class LoginPage extends StatefulWidget {
@@ -11,15 +17,39 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
-
   final logic = Get.put(LoginLogic());
   final state = Get.find<LoginLogic>().state;
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text("LoginPage"),
+    return FlutterLogin(
+      title: S.of(context).login_tile,
+      logo: 'lib/assets/images/logo.png',
+      theme: LoginTheme(
+        titleStyle: TextStyle(
+          color: Theme.of(context).colorScheme.onPrimary,
+        ),
+        cardInitialHeight: ScreenUtils.length(vertical: 20.w, horizon: 45.w),
+      ),
+      hideForgotPasswordButton: true,
+      onLogin: (data) {
+        return logic.loginEducationalSystem(data);
+      },
+      messages: LoginMessages(
+        userHint: S.of(context).login_userHint,
+        passwordHint: S.of(context).login_passwordHint,
+        loginButton: S.of(context).login_loginButton,
+        flushbarTitleError: S.of(context).login_flushbarTitleError,
+      ),
+      onRecoverPassword: (_) => Future.value(),
+      userType: LoginUserType.name,
+      userValidator: logic.userValidator,
+      passwordValidator: logic.passwordValidator,
+      onSubmitAnimationCompleted: () {
+        final logic = Get.find<AppMainLogic>();
+        logic.animationByOrientation(AppMainLogicAnimationMode.forward);
+        Get.back(id: 1);
+      },
     );
   }
 }
-
