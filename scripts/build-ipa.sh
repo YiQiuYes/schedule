0a9232d482e1c8ae81a95b1bbb7841e34de322ae
YiQiu <binzixiang@163.com>
2024-10-10 22:32:09 +0800

    feat: shorebird init

--- scripts/build-ipa.sh
+++ scripts/build-ipa.sh
@@ -2,12 +2,12 @@
 
 cd "$( cd "$( dirname "$0"  )" && pwd  )/.."
 
-flutter pub get
-flutter build ios --release --no-codesign
+# flutter pub get
+# flutter build ios --release --no-codesign
 
-cd build/ios/iphoneos
+cd build/ios/archive/Runner.xcarchive/Products/Applications
 mkdir -p Payload
 mv Runner.app Payload
 
-sh ../../../scripts/thin-payload.sh
+# sh ../../../scripts/thin-payload.sh
 zip -9 release-ios.ipa -r Payload
